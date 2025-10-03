5e196cd60edf32f656173b44707019741c35424c
Jan Orend <56254096+3dJan@users.noreply.github.com>
2025-10-04 00:06:42 +0200

    [FileNames] Update Implicit3mf path to point to RadialRadiator.3mf

--- gladius/tests/integrationtests/testdata.h
+++ gladius/tests/integrationtests/testdata.h
@@ -8,6 +8,6 @@ namespace gladius_integration_tests
         static auto constexpr SimpleGyroid = "testdata/SimpleGyroid.3mf";
         static auto constexpr F360 = "testdata/F360.3mf";
         static auto constexpr VariableVoronoi = "testdata/variable_voronoi.3mf";
-        static auto constexpr Implicit3mf = "testdata/3mf-implicit.3mf";
+        static auto constexpr Implicit3mf = "testdata/RadialRadiator.3mf";
     };
 }
